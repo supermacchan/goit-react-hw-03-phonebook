78ab34223b31f711c9709f5c3f708ed79312e334
Olga Birkovska <birkovskaya92@ukr.net>
2022-12-06 19:06:09 +0200

    add starting files

---
+++ src/components/ContactList/ContactListItem.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from './ContactListItem.module.css';
+
+export const ContactListItem = ({ contacts: {id, name, number}, onDeleteContact }) => {
+    return (
+        <li className={css.contacts__listItem} >
+            <span>{name}: {number}</span>
+                <button
+                    type="button"
+                    className={css.contacts__delBtn}
+                onClick={() => onDeleteContact(id)}
+                >
+                    Delete
+                </button>
+        </li> 
+    );
+};
+
+
+ContactListItem.propTypes = {
+    contacts: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    }).isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+};
